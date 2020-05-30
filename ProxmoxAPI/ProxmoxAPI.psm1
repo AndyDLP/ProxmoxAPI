fd669fc1a4bc6768908533808c1b71acc82f30a6
AndyDLP <andydlp93@gmail.com>
2020-05-30 09:43:23 +0100

    initial commit

---
+++ ProxmoxAPI/ProxmoxAPI.psm1
@@ -0,0 +1,22 @@
+#Requires -Version 5.0
+[cmdletbinding()]
+param()
+
+Write-Verbose $PSScriptRoot
+
+Write-Verbose 'Import everything in sub folders folder'
+foreach($folder in @('private', 'public'))
+{
+    $root = Join-Path -Path $PSScriptRoot -ChildPath $folder
+    if(Test-Path -Path $root)
+    {
+        Write-Verbose "processing folder $root"
+        $files = Get-ChildItem -Path $root -Filter *.ps1 -Recurse
+
+        # dot source each file
+        $files | where-Object{ $_.name -NotLike '*.Tests.ps1'} | 
+            ForEach-Object{Write-Verbose $_.basename; . $_.FullName}
+    }
+}
+
+Export-ModuleMember -function (Get-ChildItem -Path "$PSScriptRoot\public\*.ps1").basename
---
+++ docs/about.md
@@ -0,0 +1,5 @@
+# What is ProxmoxAPI
+
+PowerShell wrapper for the Proxmox VE REST API
+
+Authored by AndyDLP
